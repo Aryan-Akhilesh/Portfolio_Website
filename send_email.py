36e6bd9c9bc299863499f24a459e5e48c9c40ee2
Aryan Akhilesh <aryanakhil99@gmail.com>
2022-12-30 14:46:46 -0500

    added environment variable for securing password

--- send_email.py
+++ send_email.py
@@ -1,4 +1,5 @@
 import smtplib,ssl
+import os
 
 
 def send_email(message):
@@ -6,7 +7,7 @@ def send_email(message):
     port = 465
 
     username = "website.portfolio.ary@gmail.com"
-    password = "vzyizwtjhzqoavpp"
+    password = os.getenv("PASSWORD")
 
     receiver = "aryanakhil99@gmail.com"
     context = ssl.create_default_context()
